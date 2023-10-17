bba83b98ef6eaac6fa3913860705af634166da6b
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-10-17 15:08:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+songNo1 = 0;
+songNo2 = 0;
 
 function preload()
 {
@@ -40,6 +44,11 @@ function gotPoses(results)
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
 
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+
+        console.log("LeftWrist = "+ scoreLeftWrist + " , RightWrist = " + scoreRightWrist);
+
         console.log("leftWristX = "+leftWristX+ " , leftWristY = "+rightWristY);
         console.log("rightWristX = "+rightWristX+ " , rightWristY = "+rightWristY);
     }
@@ -48,4 +57,21 @@ function gotPoses(results)
 function draw()
 {
     image(video,0,0,700,500);
+    fill("blue");
+    stroke("darkblue");
+
+    songNo1 = song1.isPlaying();
+    console.log(songNo1);
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+        if(songNo1 == false)
+        {
+          song1.play();
+          document.getElementById("song_id").innerHTML = "Song name : Song1"
+        }
+        
+    }
 }
